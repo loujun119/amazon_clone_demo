82bd4614aae958c2824afeab720cdfa4cb71488b
loujun119 <46946347+loujun119@users.noreply.github.com>
2020-09-02 22:37:42 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { StateProvider } from "./StateProvider";
 import reducer, { initialState } from "./reducer";
 
 ReactDOM.render(
+  {/** React.StrictMode 在开发模式下develop，开启严格模式，运行时，执行各种检查 */}
   <React.StrictMode>
     {/** StateProvider 把component发起的action中需要的数据传到reducer中 */}
     <StateProvider initialState={initialState} reducer={reducer}>
